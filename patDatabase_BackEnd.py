d9d9ae757d310a7dc8655e0afdadea5928e6531c
RaghaviMadhwa <73604887+RaghaviMadhwa@users.noreply.github.com>
2020-11-07 16:44:19 +0530

    Update patDatabase_BackEnd.py

--- patDatabase_BackEnd.py
+++ patDatabase_BackEnd.py
@@ -18,3 +18,34 @@ def addPatRec(PID , Name, DOB, Sex, Bloodgroup, Address ,Mobile, Cash):
     cur.execute("INSERT INTO patient VALUES (NULL, ?,?,?,?,?,?,?,?)",(PID , Name, DOB, Sex, Bloodgroup, Address ,Mobile, Cash))
     con.commit()
     con.close()
+def viewData():
+    con=sqlite3.connect("patient.db")
+    cur =con.cursor()
+    cur.execute("SELECT * FROM patient")
+    row =cur.fetchall()
+    con.close
+    return row
+
+def deleteRec(id):
+    con=sqlite3.connect("patient.db")
+    cur =con.cursor()
+    cur.execute("DELETE FROM patient WHERE id=?",(id,))
+    con.commit()
+    con.close
+
+def searchData(PID="",Name="",DOB="",Sex="",Bloodgroup="",Address="",Mobile="",Cash=""):
+    con=sqlite3.connect("patient.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM patient WHERE PID=? OR Name=? OR DOB=? OR Sex=? OR Bloodgroup=? OR Address=? OR  Mobile=? OR Cash=?", (PID , Name, DOB, Sex, Bloodgroup, Address ,Mobile, Cash))
+    rows=cur.fetchall()
+    con.close()
+    return rows
+def dataUpdate(id,PID="",Name="",DOB="",Sex="",Bloodgroup="",Address="",Mobile="",Cash=""):
+    con=sqlite3.connect("patient.db")
+    cur = con.cursor()
+    cur.execute("UPDATE patient SET PID=?,Name=?,DOB=?,Sex=?,Bloodgroup=?,Address=?,Mobile=?,Cash=?, WHERE id=?", (PID , Name, DOB, Sex, Bloodgroup, Address ,Mobile, Cash,id))
+    con.commit()
+    con.close()
+
+
+patientData()
