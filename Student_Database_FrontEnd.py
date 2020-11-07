ca95818855b953c5beca5b64c1109a6723f74c19
RaghaviMadhwa <73604887+RaghaviMadhwa@users.noreply.github.com>
2020-11-07 15:33:26 +0530

    Update Student_Database_FrontEnd.py

--- Student_Database_FrontEnd.py
+++ Student_Database_FrontEnd.py
@@ -22,3 +22,31 @@ class Patient:
         Cash = StringVar()
         Address = StringVar()
         Mobile = StringVar()
+
+        def iExit():
+            iExit = tkinter.messagebox.askyesno("Patient Records System","Confirm if you want to exit")
+            if iExit > 0:
+                root.destroy()
+                return
+
+        def clearData():
+            self.txtPID.delete(0,END)
+            self.txtna.delete(0,END)
+            self.txtDOB.delete(0,END)
+            self.txtSex.delete(0,END)
+            self.txtBloodgroup.delete(0,END)
+            self.txtCash.delete(0,END)
+            self.txtAdr.delete(0,END)
+            self.txtMobile.delete(0,END)
+
+
+        def addData():
+            if(len(PID.get())!=0):
+                patDatabase_BackEnd.addPatRec(PID.get(), Name.get(),DOB.get(),Sex.get(),Bloodgroup.get(),Address.get(),Mobile.get(),Cash.get())
+                patientlist.delete(0,END)
+                patientlist.insert(END,(PID.get(), Name.get(),DOB.get(),Sex.get(),Bloodgroup.get(),Address.get(),Mobile.get(),Cash.get()))
+
+        def DisplayData():
+            patientlist.delete(0,END)
+            for row in patDatabase_BackEnd.viewData():
+              patientlist.insert(END,row,str(""))
